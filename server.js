a134d8b78370e5c035e726bc3607b9b561219c9d
HishamKhalil1990 <hisham.khalil1990@gmail.com>
2021-04-04 22:04:46 +0300

    almost done

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict'
 // environment setup
 require('dotenv').config();
-const PORT =process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 // requiering libraries
 const express = require('express');
 const superagent = require('superagent');
@@ -10,41 +10,103 @@ const cors = require('cors');
 const app = express();
 // setup app
 app.use(cors());
-app.use(express.urlencoded({extended:true})); //for passing the form data for the POST method => data in request.body
+app.use(express.urlencoded({ extended: true })); //for passing the form data for the POST method => data in request.body
 app.use(express.static('public')); // for preparing the css files and casting the html file with it
 // creating the ejs engine
-app.set('view engine','ejs'); // set engine to run the ejs files
+app.set('view engine', 'ejs'); // set engine to run the ejs files
 // listen to the port
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log('we are listening to port 3001')
 })
 // app middleware
-app.get('/',getHomePage);
-app.post('/searchs',searchPage);
-function getHomePage(request,response){
+app.get('/', getHomePage);
+app.post('/searchs', searchPage);
+function getHomePage(request, response) {
     response.render('pages/index');
 }
-function searchPage(request,response){
+function searchPage(request, response) {
     const bookSubject = request.body.search[0];
     let url = `https://www.googleapis.com/books/v1/volumes?q=${bookSubject}+`
-    if (request.body.search[0] === 'title'){
+    if (request.body.search[0] === 'title') {
         url += `in${request.body.search[1]}:`
-    }else{
+    } else {
         url += `in${request.body.search[1]}:`
     }
     superagent.get(url).then(data => {
-        const dataArr = data.body.items.map(bookObj => new Book(bookObj))
-        response.json(dataArr)
+        let dataArr = [];
+        try {
+            dataArr = data.body.items.map(bookObj => new Book(bookObj))
+        } catch (error) {
+        }
+        response.render('pages/searches/show', { dataArr: dataArr });
     })
     // response.render('pages/searches/show')
 }
-function Book(bookObj){
-    console.log(bookObj)
-    this.title = bookObj.volumeInfo.title,
-    this.author = bookObj.volumeInfo.authors.reduce((acc,auth)=>acc += auth + ",","") || '',
-    this.image = bookObj.volumeInfo.imageLinks.thumbnail || 'https://i.imgur.com/J5LVHEL.jpg',
-    this.description = bookObj.volumeInfo.description,
-    this.puplish = 'puplished in ' + bookObj.volumeInfo.publishedDate
-    this.price = bookObj.listPrice.amount + 'USD'
+function Book(bookObj) {
+    // this.title = bookObj.volumeInfo.title,
+    // this.author = bookObj.volumeInfo.authors || 'no info availble',
+    // this.image = bookObj.volumeInfo.imageLinks.thumbnail || 'https://i.imgur.com/J5LVHEL.jpg',
+    // this.description = bookObj.volumeInfo.description || 'no info availble',
+    // this.puplish = 'puplished in ' + bookObj.volumeInfo.publishedDate || 'no info availble',
+    // this.price = bookObj.listPrice.amount + 'USD' || 'no info availble'
+
+
+    this.title = exist(1, bookObj),
+        this.author = exist(2, bookObj),
+        this.image = exist(3, bookObj),
+        this.description = exist(4, bookObj),
+        this.puplish = exist(5, bookObj),
+        this.price = exist(6, bookObj)
+}
+
+function exist(index, val) {
+    let defu = "no info available";
+    let vari;
+    switch (index) {
+        case 1:
+            try {
+                vari = val.volumeInfo.title;
+            } catch (error) {
+                vari = defu;
+            }
+            break;
+        case 2:
+            try {
+                vari = val.volumeInfo.authors[0];
+            } catch (error) {
+                vari = defu;
+            }
+            break;
+        case 3:
+            defu = "https://i.imgur.com/J5LVHEL.jpg";
+            try {
+                vari = val.volumeInfo.imageLinks.thumbnail;
+            } catch (error) {
+                vari = defu;
+            }
+            break;
+        case 4:
+            try {
+                vari = val.volumeInfo.description;
+            } catch (error) {
+                vari = defu;
+            }
+            break;
+        case 5:
+            try {
+                vari = val.volumeInfo.publishedDate;
+            } catch (error) {
+                vari = defu;
+            }
+            break;
+        case 6:
+            try {
+                vari = val.listPrice.amount;
+            } catch (error) {
+                vari = defu;
+            }
+            break;
+    }
+    return vari;
 }
 
